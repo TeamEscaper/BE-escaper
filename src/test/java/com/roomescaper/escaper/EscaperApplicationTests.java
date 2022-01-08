994a9c143c06684428f87061dc99824de8b96e99
wooho <woohobi@gmail.com>
2022-01-08 18:40:43 +0900

    initial commit

---
+++ src/test/java/com/roomescaper/escaper/EscaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roomescaper.escaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
